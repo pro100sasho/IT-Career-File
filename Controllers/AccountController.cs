01b73a958350a6432c01cd8c302ec61b0cfdaec2
pro100sasho <65253560+pro100sasho@users.noreply.github.com>
2020-05-13 11:56:45 +0300

    initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Web_Stock_Market.Models;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace Web_Stock_Market.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection connetion = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+
+        }
+        public ActionResult Verify(Account account)
+        {
+            return View();
+
+        }
+    }
+}
